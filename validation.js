92219f7a1fef254e3f36726f1c325392ce81bd14
Laura Castro <lauracastrotech@gmail.com>
2023-09-26 01:02:26 -0400

    set event  to html id

--- validation.js
+++ validation.js
@@ -9,20 +9,14 @@ function validateInput(event){
     if(event.key === 'Enter'){
     // Declare variables that store the user's input
     const userName = document.getElementById("name");
-    const email = document.getElementById("email");
     
     // Declare variables that will store the value of the input
     const inputName = userName.value;
-    const inputEmail = email.value;
 
     //Define a regular expression to match only letters A-Z
     const letterRegex = /^[A-Za-z]+$/;
 
-    // Make sure that a list of characters are found in the email input
-    emailChar = ['@', '.com', '.org', '.edu']
-   //x for(let char = 0; char < inputEmail.length; char++){
-        console.log(inputEmail.splice(-4, inputEmail))
-    //}
+   
 
     }
       

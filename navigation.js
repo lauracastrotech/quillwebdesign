7bb791b54fdf96e472386816fec681cf51b63558
Laura Castro <lauracastrotech@gmail.com>
2023-09-25 22:42:44 -0400

    Changed contact form "tell me about your project..." text field to a text area

---
+++ navigation.js
@@ -0,0 +1,11 @@
+/* 
+Programmer: Laura Castro
+Date: September 25, 2023
+Purpose: Write a script to implement a behavior on the website. This is to qualify for Chingu Voyage 46. Submission is due by the 26th
+*/
+
+// Validate the data that is put in the text fields of the contact form.
+
+//When name field is typed in, check to make sure that all characters are alpha
+//If non alpha characters are found print an error to the screen and prompt use for input again
+//Check that the email field contains an '@' symbol and a '.com'

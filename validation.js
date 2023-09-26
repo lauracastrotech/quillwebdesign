0705c80ec91fcd5e92d6ffd794d16e9b993a7ae6
Laura Castro <lauracastrotech@gmail.com>
2023-09-26 00:19:06 -0400

    Added regular expression to test text field input

--- validation.js
+++ validation.js
@@ -13,46 +13,17 @@ function validateInput(event){
     
     // Declare variables that will store the value of the input
     const inputName = userName.value;
-    const inputEmail = "laura@gmail.com";
+    const inputEmail = email.value;
 
     //Define a regular expression to match only letters A-Z
+    const letterRegex = /^[A-Za-z]+$/;
 
     // Make sure that a list of characters are found in the email input
     emailChar = ['@', '.com', '.org', '.edu']
-   // for(let char = 0; char < inputEmail.length; char++){
+   //x for(let char = 0; char < inputEmail.length; char++){
         console.log(inputEmail.splice(-4, inputEmail))
     //}
 
     }
-    
-
-    //Check that the email field contains an '@' symbol and a '.com'
-
-    //Do not let user select text area field until the name and email fields are completed correctly
-
-    function checkInput(event) {
-        if (event.key === 'Enter') {
-          // Get the input field element by ID
-          const inputField = document.getElementById('textInput');
-      
-          // Get the input value
-          const inputValue = inputField.value;
-      
-          // Define a regular expression to match only letters in the English alphabet
-          const letterRegex = /^[A-Za-z]+$/;
-      
-          // Check if the input contains only letters
-          if (letterRegex.test(inputValue)) {
-            // Input is valid, clear the text field
-            inputField.value = '';
-      
-            // Display a success message
-            document.getElementById('message').textContent = 'Valid input: ' + inputValue;
-          } else {
-            // Input is invalid, display an error message
-            document.getElementById('message').textContent = 'Invalid input: Please enter only alphabetical characters.';
-          }
-        }
-      }
       
 }

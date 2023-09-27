4ff95b47ac15cbe42a6073187209ced99ac46137
Laura Castro <lauracastrotech@gmail.com>
2023-09-26 20:11:46 -0400

    troubleshooting script

--- validation.js
+++ validation.js
@@ -4,20 +4,20 @@ Date: September 25, 2023
 Purpose: Write a script to implement a behavior on the website. This is to qualify for Chingu Voyage 46. Submission is due by the 26th
 */
 
-// Validate the data that is put in the text fields of the contact form.
-function validateInput(event){
+// Add an event handler to name input field
+document.getElementById("name").addEventListener('keydown', function validateInput(event){
     if(event.key === 'Enter'){
-    // Declare variables that store the user's input
-    const userName = document.getElementById("name");
-    
-    // Declare variables that will store the value of the input
-    const inputName = userName.value;
-
-    //Define a regular expression to match only letters A-Z
-    const letterRegex = /^[A-Za-z]+$/;
-
-   
+        // Create a regular expression 
+       
+        // Take this action after the enter key is pressed down
+        var changeName = document.getElementById("name").innerHTML 
+        
+        // Prevent default behavior of form
+        event.preventDefault();
 
     }
+});
+    
+   
       
-}
+
